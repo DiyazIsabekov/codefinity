63b6b00a50da563d641993b73f0180cf13cc40ee
suzytt <sezim240298m21@mail.ru>
2025-09-23 11:25:24 +0600

    home, hero

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import CourseList from "../../Components/CourseList/CourseList";
-import s from './Home.module.scss'
-import pic1 from '../../assets/images/pic1.png'
+import Hero from "../../Components/Hero/Hero";
+
+
 
 const Home = () => {
   return (
-    <div className={s.main_screen}>
-      <div className={s.main_info}>
-        <div className={s.title}>
-          <h1>Вступайте в будущее
-            вместе с Нами</h1>
-          <button className={s.request}>ОСТАВИТЬ ЗАЯВКУ</button>
-        </div>
-        <img src={pic1} className={s.infinity} alt="infinity" />
-      </div>
+    <div>
+      <Hero />
       <CourseList />
     </div>
   );
