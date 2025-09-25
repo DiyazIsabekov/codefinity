28192710e5400a246d2c25336712d1c14cb59a6d
suzytt <sezim240298m21@mail.ru>
2025-09-25 11:59:01 +0600

    consultation, footer, study process, main, home

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import "../../App.scss";
 import DevOps from "../../Pages/DevOps/DevOps";
 import Home from "../../Pages/Home/Home";
 import BA from "../../Pages/BA/BA";
+import DA from './../../Pages/DA/DA';
+import Flutter from './../../Pages/Flutter/Flutter';
 
 const Main = () => {
   return (
-    <main>
+    <main className="container" >
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses/dev-ops" element={<DevOps />} />
         <Route path="/courses/business-analytics" element={<BA />} />
+        <Route path="/courses/data-analytics" element={<DA />} />
+        <Route path="/courses/flutter" element={<Flutter />} />
       </Routes>
     </main>
   );
--- src/Pages/BA/BA.jsx
+++ src/Pages/BA/BA.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import StudyProcess from "../../Components/StudyProcess/StudyProcess";
+import Teachers from "../../Components/Teachers/Teachers";
+import Consultation from "../../Components/Consultation/Consultation";
 
 const BA = () => {
-  return <div>BA</div>;
+  return <div>
+
+    <StudyProcess />
+    <Teachers />
+    <Consultation />
+  </div>;
 };
 
 export default BA;
