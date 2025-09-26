ddd16c64d182cc67990f397d3606f4b79c3a42ea
DiyazIsabekov <diyaz.2004@gmail.com>
2025-09-26 11:53:03 +0600

    commit fix

--- src/Pages/BA/BA.jsx
+++ src/Pages/BA/BA.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import styles from './BA.module.scss';
+import styles from "./BA.module.scss";
 import HeroDetails from "../../Components/HeroDetails/HeroDetails";
 import StudyProcess from "../../Components/StudyProcess/StudyProcess";
 import Teachers from "../../Components/Teachers/Teachers";
@@ -7,13 +7,12 @@ import Consultation from "../../Components/Consultation/Consultation";
 
 const BA = () => {
   return (
-    <div className={styles['business-analyst']}>
+    <div className={styles["business-analyst"]}>
       <HeroDetails />
-    <StudyProcess />
-    <Teachers />
-    <Consultation />
+      <StudyProcess />
+      <Teachers />
+      <Consultation />
     </div>
   );
-
-
+};
 export default BA;
