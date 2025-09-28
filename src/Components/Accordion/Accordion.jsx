dee5c5834a9f369224e65188becf2aec6e28c45b
IreneFox2025 <bronnikova702@gmail.com>
2025-09-28 13:37:26 +0600

    accordion has been finished

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -18,30 +18,74 @@ const coursesData = [
   theme_title: 'Темы:',
   text: 'Почему IT-компании не выжить без бизнес-аналитика? Разбираемся, какую роль он выполняет и как именно сохраняет бизнесу деньги. Знакомимся с профессиями в IT, рассматриваем жизненный цикл разработки программного обеспечения',
   bullit1: 'Введение в курс',
+  bullit2: 'Основы бизнес-анализа',
+  bullit3: 'Ключевые понятия бизнес-анализа',
+  bullit4: 'Управление заинтересованными сторонами',
+  bullit5: 'Требования. Атрибуты качества требований',
+  bullit6: 'Жизненный цикл разработки программ (SDLC)',
+  bullit7: 'Аспект бизнес-анализа в разработке',
+  bullit8: 'Планирование и мониторинг бизнес-анализа',
+  bullit9: '+1 практическая работа',
 },
 {
   id: 2,
   title: 'Модуль 2',
   theme_title: 'Темы:',
   text: "We all are flying on our meat spacecrafts through the time and the space of the infinity universe",
+  bullit1: 'eniki',
+  bullit2: 'beniki',
+  bullit3: 'yeli',
+  bullit4: 'vareniki',
+  bullit5: '',
+  bullit6: '',
+  bullit7: '',
+  bullit8: '',
+  bullit9: '',
 },
 { 
   id: 3,
   title: 'Модуль 3',
   theme_title: 'Темы:',
   text: "And we all will definetily die",
+  bullit1: 'от топота',
+  bullit2: 'копыт',
+  bullit3: 'пыль по полю',
+  bullit4: 'летит',
+  bullit5: '',
+  bullit6: '',
+  bullit7: '',
+  bullit8: '',
+  bullit9: '',
 },
 { 
   id: 4,
   title: 'Soft skills',
   theme_title: 'Темы:',
   text: "So let's celebrate our life now",
+  bullit1: 'лавировали',
+  bullit2: 'лавировали',
+  bullit3: 'да не вылавировали',
+  bullit4: '',
+  bullit5: '',
+  bullit6: '',
+  bullit7: '',
+  bullit8: '',
+  bullit9: '',
 },
 { 
   id: 5,
   title: 'Hard skills',
   theme_title: 'Темы:',
   text: "Amen",
+  bullit1: 'мороз',
+  bullit2: 'и солнце',
+  bullit3: 'день чудесный',
+  bullit4: 'в лесу',
+  bullit5: 'раздавался',
+  bullit6: 'топор',
+  bullit7: 'дровосека',
+  bullit8: 'им дровосек',
+  bullit9: 'отгонял',
 },
 
 ]
@@ -56,7 +100,8 @@ export default function ControlledAccordions() {
 
   return (
     <div>
-        {coursesData.map((el, i) => (
+        {
+        coursesData.map((el, i) => (
               
       <Accordion 
            className='accordion' 
@@ -90,11 +135,11 @@ export default function ControlledAccordions() {
             expandIcon={
             expandedId === el.id ? (
             <img 
-            src= {arr_down} alt="expand" style={{ width: 50, height: 44, margin: 20 }} 
+            src= {arr_up} alt="expand" className={s.expandIcon} style={{ width: 50, height: 44, margin: 20 }} 
           />
           ) : (
             <img
-              src={arr_up} alt="collapse" style={{ width: 50, height: 44, margin: 20 }}
+              src={arr_down} alt="collapse" className={s.expandIcon} style={{ width: 50, height: 44, margin: 20 }}
             />
           )
           }
@@ -113,6 +158,12 @@ export default function ControlledAccordions() {
           margin: 0,
           padding: 0,
           },
+           "& .MuiAccordionSummary-expandIconWrapper": {
+          position: "absolute",
+          right: "16px",
+          top: "16px",
+          margin: 0,
+          },
           }}
           aria-controls="panel1bh-content"
           id="panel1bh-header"
@@ -128,6 +179,11 @@ export default function ControlledAccordions() {
             {el.theme_title}
           </Typography>
         </Box>
+        {/* <img
+         src={expandedId === el.id ? arr_down : arr_up}
+         alt="toggle"
+         className={s.expandIcon}
+        /> */}
       </Box>
         </AccordionSummary>
 
@@ -148,17 +204,12 @@ export default function ControlledAccordions() {
           </Box>
           <Box className={s.module_right}>
           <Typography className={s.right_text}>
-            {/* <div>
-            <p>Введение в курс</p>
-            <p>Основы бизнес-анализа</p>
-            </div>
-            <p>Ключевые понятия бизнес-анализа</p>
-            <p>Управление заинтересованными сторонами</p>
-            <p>Требования. Атрибуты качества требований</p>
-            <p>Жизненный цикл разработки программ (SDLC)</p>
-            <p>Аспект бизнес-анализа в разработке</p>
-            <p>Планирование и мониторинг бизнес-анализа</p>
-            <p>+1 практическая работа</p> */}
+            <div className={s.bullits_wrapper}>
+              {[el.bullit1, el.bullit2, el.bullit3, el.bullit4, el.bullit5, el.bullit6, el.bullit7, el.bullit8, el.bullit9]
+              .filter(el => el && el.trim() !== "")
+              .map((el, i) => <span key={i}>{el}</span>)
+               }
+             </div>
           </Typography>
           </Box>
           </Box>
