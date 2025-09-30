55bfeb36db20f985ae042fde72d485cfb23d849a
DiyazIsabekov <diyaz.2004@gmail.com>
2025-09-30 14:17:04 +0600

    framer motion

--- src/Components/CourseCard/CourseCard.jsx
+++ src/Components/CourseCard/CourseCard.jsx
@@ -1,23 +1,36 @@
 import React from "react";
-import s from './Course.module.scss'
+import s from "./Course.module.scss";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const CourseCard = ({ title, color, imageLink, url }) => {
   const navigate = useNavigate();
 
   const handleNavigate = () => {
-    navigate(url)
-  }
+    navigate(url);
+  };
 
   return (
-    <div className={s.course_card} style={{ backgroundColor: color, color: color }}>
+    <motion.div
+      className={s.course_card}
+      style={{ backgroundColor: color, color: color }}
+      variants={{
+        hidden: { opacity: 0 },
+        show: {
+          opacity: 1,
+          transition: {
+            staggerChildren: 0.25,
+          },
+        },
+      }}
+    >
       <div className={s.logo_wrap}>
         <img src={imageLink} alt="" />
       </div>
       <h3>{title}</h3>
       <button onClick={handleNavigate}>Подробнее</button>
-    </div>
-  )
+    </motion.div>
+  );
 };
 
 export default CourseCard;
