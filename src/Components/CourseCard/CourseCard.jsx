aaf8a9f3c0ff2fbd43bfab44af953211ef055676
suzytt <sezim240298m21@mail.ru>
2025-10-02 20:01:45 +0600

    motion and adaptive courselist, coursecard, commit hero, header

--- src/Components/CourseCard/CourseCard.jsx
+++ src/Components/CourseCard/CourseCard.jsx
@@ -10,26 +10,34 @@ const CourseCard = ({ title, color, imageLink, url }) => {
     navigate(url);
   };
 
+
+  const itemVariants = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 0.5 } },
+  };
+
+  const infoVariants = {
+    hidden: { opacity: 0, x: -25 },
+    show: { opacity: 1, x: 0, transition: { duration: 0.6 } },
+  }
+
+
   return (
     <motion.div
+      variants={itemVariants}
+
       className={s.course_card}
       style={{ backgroundColor: color, color: color }}
-      variants={{
-        hidden: { opacity: 0 },
-        show: {
-          opacity: 1,
-          transition: {
-            staggerChildren: 0.25,
-          },
-        },
-      }}
     >
-      <div className={s.logo_wrap}>
+
+      <motion.div
+        variants={infoVariants}
+        className={s.logo_wrap}>
         <img src={imageLink} alt="" />
-      </div>
-      <h3>{title}</h3>
-      <button onClick={handleNavigate}>Подробнее</button>
-    </motion.div>
+      </motion.div>
+      <motion.h3 variants={infoVariants}>{title}</motion.h3>
+      <motion.button variants={infoVariants} onClick={handleNavigate}>Подробнее</motion.button>
+    </motion.div >
   );
 };
 
