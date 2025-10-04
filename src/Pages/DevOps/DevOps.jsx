7d6149648d2c9091c687a8688fe8dc538ce6eeea
Ksonya24 <sofa.konoplya2015@gmail.com>
2025-10-04 10:07:32 +0600

    accent section

--- src/Pages/DevOps/DevOps.jsx
+++ src/Pages/DevOps/DevOps.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./DevOps.module.scss";
 import HeroDetails from "../../Components/HeroDetails/HeroDetails";
 import Container from "../../Components/Container/Container";
+import Accent from "../../Components/AccentSection/Accent";
 
 const DevOps = () => {
   return (
     <div className={styles['dev-ops']}>
       <HeroDetails />
+      <Accent />
       <Container className={styles['dev-ops__container']}>
         sections
       </Container>
