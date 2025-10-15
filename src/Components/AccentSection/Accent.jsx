07d108921d80d4b72a9f55fa29c4038a590abf47
Arsa Ismail <arsaismail050505@gmail.com>
2025-10-15 10:34:50 +0600

    added accent

--- src/Components/AccentSection/Accent.jsx
+++ src/Components/AccentSection/Accent.jsx
@@ -13,31 +13,33 @@ const Accent = () => {
                 <div className={s.items_wrap}>
                     <div className={s.accent_items}>
                         <div className={s.accent_item}>
-                            <img src={practice} alt="practice" />
-                            <div className={s.text_wrap}>
-                                <h4 className={s.accent_percent}>70%</h4>
-                                <p className={s.text}>практика</p>
+                            <div className={s['acent__top-block']}>
+                                <img src={practice} alt="practice" />
+                                <div className={s.text_wrap}>
+                                    <h4 className={s.accent_percent}>70%</h4>
+                                    <p className={s.text}>практика</p>
+                                </div>
+                            </div>
+                            <div className={s['acent__bottom-block']}>
+                                <p id='my_text' className={s.text_base}>Работы в портфолио</p>
+                                <p className={s.text_base}>Командные проекты</p>
+                                <p className={s.text_base}>Личные проекты</p>
                             </div>
                         </div>
                         <div className={s.accent_item}>
-                            <img src={theory} alt="theory" />
-                            <div className={s.text_wrap}>
-                                <h4 className={s.accent_percent}>10%</h4>
-                                <p className={s.text}>теория</p>
+                            <div className={s['acent__top-block']}>
+                                <img src={theory} alt="theory" />
+                                <div className={s.text_wrap}>
+                                    <h4 className={s.accent_percent}>10%</h4>
+                                    <p className={s.text}>теория</p>
+                                </div>
+                            </div>
+                            <div className={s['acent__bottom-block']}>
+                                <p id='my_text' className={s.text_base}>Работы в портфолио</p>
+                                <p className={s.text_base}>Командные проекты</p>
+                                <p className={s.text_base}>Личные проекты</p>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className={s.accent_wrap}>
-                    <div className={s.left_block}>
-                        <p id='my_text' className={s.text_base}>Работы в портфолио</p>
-                        <p className={s.text_base}>Командные проекты</p>
-                        <p className={s.text_base}>Личные проекты</p>
-                    </div>
-                    <div className={s.right_block}>
-                        <p className={s.text_base}>Презентации по всем темам</p>
-                        <p className={s.text_base}>Вебинары</p>
-                        <p className={s.text_base}>Шаблоны, подборки, чеклисты</p>
                     </div>
                 </div>
             </div>
